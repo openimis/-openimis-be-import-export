0be081b629a6e255723b3b7ba03c1da1a653d8f6
Kamil Malinowski <malinowskikam96@gmail.com>
2023-03-07 10:49:40 +0100

    OTC-922 Update insuree import-export API to match existing fe components (#2)
    * OTC-922 Updated im_export API
    
    * OTC-922 Added support for strategy and dry_run
    
    * OTC-922 Fixed tests

--- im_export/views.py
+++ im_export/views.py
@@ -1,12 +1,13 @@
-import traceback
-
+import json
+import logging
+from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from tablib import Dataset
-
 from insuree.apps import InsureeConfig
-from .resources import InsureeResource
+from .services import InsureeImportExportService
+
+logger = logging.getLogger(__name__)
 
 
 def check_user_rights(rights):
@@ -20,40 +21,38 @@ def check_user_rights(rights):
 @api_view(["POST"])
 @permission_classes([check_user_rights(InsureeConfig.gql_mutation_create_insurees_perms, )])
 def import_insurees(request):
-    import_file = request.FILES.get('file', None)
-
-    # todo standarize messages
-    if not import_file:
-        return Response({'success': False, 'message': 'No import file provided'})
-
-    # todo check regions of current user to forbid adding insurees to non assigned users
-    resource = InsureeResource(request.user)
-
     try:
-        data_set = resource.validate_and_sort_dataset(
-            Dataset(headers=InsureeResource.insuree_headers).load(import_file.read().decode()))
+        import_file = request.FILES.get('file', None)
+        user = request.user
+        dry_run = json.loads(request.POST.get('dry_run', 'false'))
+        strategy = request.POST.get('strategy', InsureeImportExportService.Strategy.INSERT)
+
+        success, totals, errors = InsureeImportExportService(user) \
+            .import_insurees(import_file, dry_run=dry_run, strategy=strategy)
+        return JsonResponse(data={'success': success, 'data': totals, 'errors': errors})
+    except ValueError as e:
+        logger.error("Error while importing insurees", exc_info=e)
+        return Response({'success': False, 'error': str(e)}, status=400)
     except Exception as e:
-        # todo for debug, log properly
-        traceback.print_exc()
-        return Response({'success': False, 'message': 'Failed to parse import file', 'details': str(e)})
-
-    if data_set:
-        data_set = data_set.sort('head_insuree_number')
-        result = resource.import_data(data_set, dry_run=True)  # Test the data import
-
-        if not result.has_errors():
-            resource.import_data(data_set, dry_run=False)  # Actually import
-            return Response({'success': True})
-        else:
-            # todo add proper error reporting, add errors from rows
-            return Response({'success': False, 'message': 'Import file contains errors',
-                             'details': [str(error.error) for error in result.base_errors]})
-    else:
-        return Response({'success': False, 'message': 'No rows to import'})
+        logger.error("Unexpected error while importing insurees", exc_info=e)
+        return Response({'success': False, 'error': str(e)}, status=500)
 
 
 @api_view(["GET"])
 @permission_classes([check_user_rights(InsureeConfig.gql_query_insurees_perms, )])
 def export_insurees(request):
-    # TODO add location based filtering
-    return Response(InsureeResource(request.user).export().dict)
+    try:
+        # TODO add location based filtering
+        export_format = request.GET.get("file_format", "csv")
+        user = request.user
+
+        content_type, export = InsureeImportExportService(user).export_insurees(export_format)
+        response = HttpResponse(export, content_type=content_type)
+        response['Content-Disposition'] = f'attachment; filename="insurees.{export_format}"'
+        return response
+    except ValueError as e:
+        logger.error("Error while exporting insurees", exc_info=e)
+        return Response({'success': False, 'error': str(e)}, status=400)
+    except Exception as e:
+        logger.error("Unexpected error while exporting insurees", exc_info=e)
+        return Response({'success': False, 'error': str(e)}, status=500)
